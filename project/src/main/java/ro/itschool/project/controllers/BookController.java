9ed51a99ac2c87185111f48bc9a5cc61247656d2
Cezar Nedelcu <cezarnedelcu@gmail.com>
2023-11-07 21:24:38 +0200

    session20 practice

---
+++ project/src/main/java/ro/itschool/project/controllers/BookController.java
@@ -0,0 +1,30 @@
+package ro.itschool.project.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ro.itschool.project.models.Book;
+import ro.itschool.project.services.BookService;
+
+import java.util.List;
+@RequestMapping("/api/books")
+@RestController
+public class BookController {
+
+    //create REST APIs for CRUD operations for Book resource
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        return ResponseEntity.ok(bookService.createBook(book));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Book>> getAllBooks(){
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+}
---
+++ project/src/main/java/ro/itschool/project/services/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package ro.itschool.project.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ro.itschool.project.models.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class BookServiceImpl implements BookService {
+
+    List<Book> bookList = new ArrayList<>();
+
+    @Override
+    public Book createBook(Book book) {
+        if (!isValid(book)) {
+            throw new RuntimeException("Invalid input");
+        }
+        long bookId = bookList.size() + 1L;
+        book.setId(bookId);
+
+        bookList.add(book);
+        log.info("Book with id {} has been created", bookId);
+
+        return bookList.get((int) bookId - 1);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        log.info("Retrived book list.");
+        return bookList;
+    }
+
+    private boolean isValid(Book book) {
+        return !book.getTitle().isEmpty()
+                && !book.getAuthor().isEmpty()
+                && book.getPrice() != null;
+    }
+}
