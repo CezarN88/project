17aad0d33407e5f1f48660c8e1754070ec5cc0a6
Cezar Nedelcu <cezarnedelcu@gmail.com>
2023-11-10 21:03:28 +0200

    session22 practice

---
+++ project/src/main/java/ro/itschool/project/services/WeatherValidatorService.java
@@ -0,0 +1,9 @@
+package ro.itschool.project.services;
+
+public interface WeatherValidatorService {
+
+   public void  validateCity(String city) ;
+
+
+
+}
---
+++ project/src/main/java/ro/itschool/project/services/WeatherValidatorServiceImpl.java
@@ -0,0 +1,12 @@
+package ro.itschool.project.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherValidatorServiceImpl implements WeatherValidatorService {
+
+    @Override
+    public void validateCity(String city) {
+
+    }
+}
